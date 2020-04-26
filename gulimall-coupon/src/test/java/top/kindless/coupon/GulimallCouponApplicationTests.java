f6190ca82f6667564d49716e2059f8650655f95d
kindless <2411347677@qq.com>
2020-04-26 16:12:46 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/top/kindless/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package top.kindless.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
